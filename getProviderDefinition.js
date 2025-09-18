192ee487c9365c06647ff5139114573e4d60fac2
cian chambliss <cian@alphasoftware.com>
2025-09-18 14:28:54 -0400

    Added metadata / moved textPrompt out

---
+++ getProviderDefinition.js
@@ -0,0 +1,50 @@
+module.exports = function (provider) {
+        const handlers = {
+        //-------------------------------------------------------------------------------------------
+        // OLLAMA AI text prompt driver
+        "ollama": function () {
+            return require("./definitions/ollama.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // OPENAI text prompt driver
+        "openai": function () {
+            return require("./definitions/openai.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // google text prompt driver
+        "google": function () {
+            return require("./definitions/google.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // google vextex text prompt driver - documentation says this should work with service accounts
+        "google-vertex": function () {
+            return require("./definitions/google.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // Anthropic text prompt driver
+        "anthropic": function ()  {
+            return require("./definitions/anthropic.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // Groq text prompt driver
+        "groq": function () {
+             return require("./definitions/groq.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // XAI text prompt driver
+        "xai": function () {
+             return require("./definitions/xai.json");
+        },
+        //-------------------------------------------------------------------------------------------
+        // Mistral  text prompt driver
+        "mistral": function () {
+             return require("./definitions/mistral.json");
+        }
+    };
+    // Resolve the provider and call the specific dynamic handler
+    if (handlers[provider]) {
+        return handlers[provider]();
+    } else {
+        return { "error" : "Provider not recognized as supported"};
+    }
+};
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 module.exports = function () {
+    const getProviderDefintion = require("./getProviderDefinition"); 
     const textPrompt = require("./textPrompt"); 
+    const listProviders = function () {
+        // Currently support providers
+        return [
+            "ollama",
+            "openai",
+            "google",
+            "google-vertex",
+            "anthropic",
+            "groq",
+            "xai",
+            "mistral"
+        ];
+    };
     const ai = {
-        textPrompt: textPrompt
+        textPrompt: textPrompt ,
+        listProviders: listProviders,
+        getProviderDefintion: getProviderDefintion
     };
     return ai;
 };
